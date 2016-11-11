fd0bf54fef8830a09e49c47f57a8a2d996ce5b41
jie <fgnna@zuqiukong.com>
2016-11-11 17:12:59 +0800

    增加容错处理，增加代理连接判断

--- testtime.py
+++ testtime.py
@@ -3,4 +3,4 @@ import urllib2
 data = urllib2.urlopen("https://scontent.cdninstagram.com/t51.2885-15/e35/14607129_209985206092979_2689480040827060224_n.jpg?ig_cache_key=MTM3ODQxMjU4MTg5MTY2MjI2MA%3D%3D.2").read()
 img_file = open('./temp/temp.jpg', 'w')
 img_file.write(data)
-img_file.close() 
+img_file.close()
